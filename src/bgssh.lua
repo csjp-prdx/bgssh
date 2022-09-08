a15f36f1b5ec32f118574d2cb71025d671a97983
csjp-prdx <conrad.park@paradox.ai>
2022-09-08 13:26:19 -0500

    Added to the main lua file.

--- src/bgssh.lua
+++ src/bgssh.lua
@@ -1,3 +1,36 @@
 require "dir"
 
-local file = SelectFile()
+local CONF = "/var/tmp/bgssh.conf"
+
+function ReadConf()
+    local file = io.open(CONF, "r")
+    local ret
+
+    if file then
+        for line in io.lines(CONF) do
+            print(line)
+        end
+
+        io.close(file)
+        ret = true
+    else
+        print("File <bgssh.conf> does not exist.")
+        ret = false
+    end
+
+    return ret
+end
+
+function WriteConf()
+    return false
+end
+
+function Main()
+    local usr_info = {}
+
+    if ReadConf() == false then
+    end
+
+    -- Key = string.gsub(Key, '$HOME', HOME)
+    WriteConf()
+end
